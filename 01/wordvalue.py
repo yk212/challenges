f73793ec9326b1869fc7418a73b8477c0218d20f
yogev <yogev.kr@gmail.com>
2018-04-23 11:44:30 +0300

    Coding standards

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,104 +1,118 @@
 from data import DICTIONARY, LETTER_SCORES
 
+
 class SignatureDictionary:
     dic = None
+
 
 def load_words():
     """Load dictionary into a list and return list"""
 
-    with open(DICTIONARY) as dictionaryFile:
-        dictionaryLines = dictionaryFile.readlines()
+    with open(DICTIONARY) as dictionary_file:
+        dictionary_lines = dictionary_file.readlines()
 
-    dictionaryWords = [line.rstrip() for line in dictionaryLines]
+    dictionary_words = [line.rstrip() for line in dictionary_lines]
 
-    return dictionaryWords
+    if not dictionary_words:
+        raise ValueError('The dictionary is Empty')
+
+    return dictionary_words
+
 
 def calc_word_value(i_word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
 
-    wordValue = 0
+    word_value = 0
     for letter in i_word.upper():
-        if LETTER_SCORES.has_key(letter):
-            wordValue += LETTER_SCORES[letter]
+        if letter in LETTER_SCORES:
+            word_value += LETTER_SCORES[letter]
 
-    return wordValue
+    return word_value
 
-def max_word_value(i_words = load_words()):
+
+def max_word_value(i_words=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
 
-    maxWord = max(i_words, key=calc_word_value)
-    return maxWord
+    if not i_words:
+        i_words = load_words()
+
+    max_word = max(i_words, key=calc_word_value)
+    return max_word
 
 
 def init_find_optimal():
 
-    newDictionary = {}
-    dictionaryWords = load_words()
+    new_dictionary = {}
+    dictionary_words = load_words()
 
-    for word in dictionaryWords:
-        sign = get_word_signatue(word)
+    for word in dictionary_words:
+        sign = get_word_signature(word)
 
-        if newDictionary.has_key(sign):
-           newDictionary[sign].append(word)
+        if new_dictionary.has_key(sign):
+           new_dictionary[sign].append(word)
         else:
-            newDictionary[sign] = [word]
+            new_dictionary[sign] = [word]
 
-    SignatureDictionary.dic = newDictionary
+    SignatureDictionary.dic = new_dictionary
 
-def get_word_signatue(word):
+
+def get_word_signature(word):
     return ''.join(sorted(word)).upper()
 
+
 def get_substrings_of_exact_score(i_list, i_score):
-    currentList = []
+    current_list = []
     global_list = []
-    get_substrings_of_specific_score_r(global_list, list(i_list), len(i_list), currentList, i_score)
+    get_substrings_of_specific_score_r(global_list, list(i_list), len(i_list), current_list, i_score)
     return set(global_list)
 
-def get_substrings_of_specific_score_r(output_list, i_list, i_listLength, i_subList, i_score):
+
+def get_substrings_of_specific_score_r(i_output_list, i_list, i_list_length, i_sub_list, i_score):
     if i_score == 0:
-        output_list.append(get_word_signatue(i_subList))
+        i_output_list.append(get_word_signature(i_sub_list))
         return
 
-    if i_listLength == 0:
+    if i_list_length == 0:
         return
 
-    if calc_word_value(i_list[i_listLength - 1]) <= i_score:
-        get_substrings_of_specific_score_r(output_list, i_list, i_listLength - 1, i_subList, i_score)
-        get_substrings_of_specific_score_r(output_list, i_list, i_listLength - 1, i_subList + [i_list[i_listLength - 1]],
-                                           i_score - calc_word_value(i_list[i_listLength - 1]))
+    if calc_word_value(i_list[i_list_length - 1]) <= i_score:
+        get_substrings_of_specific_score_r(i_output_list, i_list, i_list_length - 1, i_sub_list, i_score)
+        get_substrings_of_specific_score_r(i_output_list, i_list, i_list_length - 1, i_sub_list + [i_list[i_list_length - 1]],
+                                           i_score - calc_word_value(i_list[i_list_length - 1]))
     else:
-        get_substrings_of_specific_score_r(output_list, i_list, i_listLength - 1, i_subList, i_score)
+        get_substrings_of_specific_score_r(i_output_list, i_list, i_list_length - 1, i_sub_list, i_score)
 
 
-def find_optimal(i_bunchOfLetters):
+def find_optimal(i_bunch_of_letters):
     """Find the optimal (highest scoring) word from these letters (including substrings) which is in the dictionary.
     Return it and the score."""
 
-    if SignatureDictionary.dic == None:
+    if not SignatureDictionary.dic:
         init_find_optimal()
 
     # Calc the maximum score
-    maxPossibleScore = calc_word_value(i_bunchOfLetters)
+    max_possible_score = calc_word_value(i_bunch_of_letters)
 
     # Run from maximum score to 0
 
-    for currentScore in reversed(range(maxPossibleScore + 1)):
+    for current_score in reversed(range(max_possible_score + 1)):
 
         # Find all permutation with this score
-        currentScoreWordList = get_substrings_of_exact_score(i_bunchOfLetters, currentScore)
+        current_score_word_list = get_substrings_of_exact_score(i_bunch_of_letters, current_score)
 
         # For each permutation check if exist in dic
-        for word in currentScoreWordList:
+        for word in current_score_word_list:
             if (SignatureDictionary.dic.has_key(word)):
-                identicSignatureWords = SignatureDictionary.dic[word]
-                maxWord = identicSignatureWords[0]
-                maxWordValue = calc_word_value(maxWord)
-                return (maxWord, maxWordValue)
+                identic_signature_words = SignatureDictionary.dic[word]
+                maxWord = identic_signature_words[0]
+                max_word_value = calc_word_value(maxWord)
+                return (maxWord, max_word_value)
 
     return (None, 0)
 
 
 if __name__ == "__main__":
+    print find_optimal("aaavvsdvadfasdfadfsdfadgrynj")
     pass # run unittests to validate
